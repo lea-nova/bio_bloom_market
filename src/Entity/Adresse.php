8ef10dc8b26b27ff2de861cc31164c42246ad058
lea-nova <lea.rulliere@outlook.fr>
2024-05-25 22:10:14 +0200

    Création de l'entité Adresse et du CRUD

---
+++ src/Entity/Adresse.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity()]
+class Adresse
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ligne1 = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $ligne2 = null;
+
+    #[ORM\Column(length: 5)]
+    private ?string $codePostal = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $ville = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $pays = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLigne1(): ?string
+    {
+        return $this->ligne1;
+    }
+
+    public function setLigne1(string $ligne1): static
+    {
+        $this->ligne1 = $ligne1;
+
+        return $this;
+    }
+
+    public function getLigne2(): ?string
+    {
+        return $this->ligne2;
+    }
+
+    public function setLigne2(?string $ligne2): static
+    {
+        $this->ligne2 = $ligne2;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(string $codePostal): static
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): static
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): static
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+}
