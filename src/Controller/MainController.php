f8183543837cdd674e4eeb80c79b3222576e10ae
lea-nova <lea.rulliere@outlook.fr>
2024-12-24 17:21:34 +0100

    Création du dashboard Admin pour accéder aux informations concernant al table Fournisseur

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
@@ -17,4 +19,14 @@ class MainController extends AbstractController
             'controller_name' => 'MainController',
         ]);
     }
+
+    #[Route('/admin', name: 'admin_app_main',  methods: ['GET'])]
+    public function dashboard(): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) { // Redirige vers la route de la page d'accueil 
+            return $this->redirectToRoute('app_main');
+        }
+        return $this->render('admin/admin_home.html.twig');
+    }
+    // return $this->redirectToRoute('app_main', []);
 }
